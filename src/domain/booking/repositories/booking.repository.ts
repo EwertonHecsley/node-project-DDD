b8e6ec446987c9a5a227582b62f13322b2b8eb65
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-08-24 12:10:49 -0300

    get booking

--- src/domain/booking/repositories/booking.repository.ts
+++ src/domain/booking/repositories/booking.repository.ts
@@ -4,4 +4,5 @@ import Booking from "../entities/booking.entity";
 export abstract class BookingRepository {
     abstract create(room: Booking): Promise<Booking>;
     abstract findMany(): Promise<Booking[]>;
+    abstract findById(id: string): Promise<Booking | null>;
 }
