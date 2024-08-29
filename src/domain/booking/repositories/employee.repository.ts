cca3b1aa76e5f50d5eb715bb47ca8249d9ee8c36
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-08-28 21:13:22 -0300

    create new employee

--- src/domain/booking/repositories/employee.repository.ts
+++ src/domain/booking/repositories/employee.repository.ts
@@ -4,6 +4,6 @@ export abstract class EmployeeRepository {
     abstract create(employee: Employee): Promise<Employee>;
     abstract findMany(): Promise<Employee[]>;
     abstract findById(id: string): Promise<Employee | null>;
-    abstract findByEmail(employee: Employee): Promise<Employee | null>
+    abstract findByEmail(email: string): Promise<Employee | null>
     abstract cancel(employee: Employee): Promise<void>
 }
